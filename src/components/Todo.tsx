a18479c94fad650eaab57120955a3ceadb284ba9
Akash <aakash@hey.com>
2020-08-29 20:49:55 +0800

    extract todo item into it's component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
-
+import Clock from "./Clock";
+import TodoItem from "./TodoItem";
 import useUnsplash from "../hooks/useUnsplash";
 import useTodo, { ItemType } from "../hooks/useTodo";
-import Clock from "./Clock";
 
 export default function Todo(props: any) {
   const [imgUrl] = useUnsplash();
@@ -32,59 +32,13 @@ export default function Todo(props: any) {
           className={"box-content mt-2 max-w-full mx-auto p-8 overflow-y-auto"}
           style={{ maxHeight: "50vh" }}
         >
-          {items.map((item: ItemType) => {
-            return (
-              <div
-                key={item.id}
-                className={
-                  "flex z-10 max-w-sm items-center justify-between text-2xl border border-gray-300 cursor-pointer shadox-xl p-4 mx-auto mt-2 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-                }
-                onClick={(e) => toggleCheck(item)}
-              >
-                <div className={"pr-2"}>
-                  <svg
-                    className="h-6 w-6 border border-gray-300 rounded-md"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    {item.checked && (
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    )}
-                  </svg>
-                </div>
-                <div
-                  className={item.checked ? "line-through" : ""}
-                  style={{ wordBreak: "break-word" }}
-                >
-                  {item.text}
-                </div>
-                <div
-                  className={
-                    "ml-2 hover:rounded-full hover:bg-gray-300 hover:bg-opacity-25 hover:text-gray-900 "
-                  }
-                  onClick={(e) => deleteItem(item)}
-                >
-                  <svg
-                    className="h-6 w-6 "
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-              </div>
-            );
-          })}
+          {items.map((item: ItemType) => (
+            <TodoItem
+              key={item.id}
+              item={item}
+              hooks={{ toggleCheck, deleteItem }}
+            />
+          ))}
         </div>
       </div>
     </div>
